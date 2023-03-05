884b10216125a7523dedfbc833db1529477e253f
dgmolokov <dg.molokov@gmail.com>
2023-03-05 13:07:07 +0700

    added logger

--- src/level2/lesson2/homework/Main.java
+++ src/level2/lesson2/homework/Main.java
@@ -2,14 +2,20 @@ package level2.lesson2.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Main {
+  private static final Logger log = Logger.getLogger("Main");
   public static void main(String[] args) {
     var lists = new ArrayList<List<Integer>>();
     for (int i = 0; i < 4; i++) {
       lists.add(List.of(1, 2, 3, 4));
     }
-    System.out.println(sum(lists));
+    try {
+      System.out.println(sum(lists));
+    } catch (MyArraySizeException e) {
+      log.info(e.getMessage());
+    }
   }
 
   private static int sum(ArrayList lists) {
