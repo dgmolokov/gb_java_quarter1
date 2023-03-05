d8a5302bec3e1e7a30eb23202ba25531b0557bd4
dgmolokov <dg.molokov@gmail.com>
2023-03-05 13:50:45 +0700

    returned MyArrayDataException, deleted sum method and added getSumOfMatrixElements and getSumOfList methods

--- src/level2/lesson2/homework/Main.java
+++ src/level2/lesson2/homework/Main.java
@@ -7,27 +7,36 @@ import java.util.logging.Logger;
 public class Main {
   private static final Logger log = Logger.getLogger("Main");
   public static void main(String[] args) {
-    var lists = new ArrayList<List<Integer>>();
+    var lists = new ArrayList<List>();
     for (int i = 0; i < 4; i++) {
       lists.add(List.of(1, 2, 3, 4));
     }
     try {
-      System.out.println(sum(lists));
-    } catch (MyArraySizeException e) {
+      System.out.println(getSumOfMatrixElements(lists));
+    } catch (MyArraySizeException | MyArrayDataException e) {
       log.info(e.getMessage());
     }
   }
 
-  private static int sum(ArrayList lists) {
-    int count = 0;
-    if (lists.size() != 4) throw new MyArraySizeException("Список должен быть размером 4x4!");
-    for (int i = 0; i < 4; i++) {
-      var list = (List<Integer>) lists.get(i);
-      if (list.size() != 4) throw new MyArraySizeException("Список должен быть размером 4x4!");
-      for (int j = 0; j < 4; j++) {
-        count += list.get(j);
+  private static int getSumOfMatrixElements(ArrayList<List> lists) {
+    if (lists.size() != 4) throw new MyArraySizeException("Количество списков должно быть равно четырем!");
+    int sum = 0;
+    try {
+      for (List list: lists) {
+        sum += getSumOfList(list);
       }
+      return sum;
+    } catch (ClassCastException e) {
+      throw new MyArrayDataException("В списках найдено что-то кроме чисел!");
     }
-    return count;
+  }
+
+  private static int getSumOfList(List<Integer> list) {
+    if (list.size() != 4) throw new MyArraySizeException("Количество элементов в списке должно быть равно четырем!");
+    int sum = 0;
+    for (int i: list) {
+      sum += i;
+    }
+    return sum;
   }
 }
---
+++ src/level2/lesson2/homework/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package level2.lesson2.homework;
+
+public class MyArrayDataException extends RuntimeException {
+  public MyArrayDataException(String message) {
+    super(message);
+  }
+}
