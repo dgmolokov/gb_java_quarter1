902184092ff3000d54b325c4adece707ff162432
dgmolokov <dg.molokov@gmail.com>
2023-03-14 14:35:19 +0700

    moved math operations to "calculate" method, added logger

--- src/level2/lesson5/homework/Main.java
+++ src/level2/lesson5/homework/Main.java
@@ -1,10 +1,21 @@
 package level2.lesson5.homework;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+  private static final Logger LOGGER = Logger.getLogger("Main");
   static final int SIZE = 10_000_000;
   static final int HALF = SIZE / 2;
+
+
+  private static void calculate(ArrayList<Float> list) {
+    for (int i = 0; i < list.size(); i++) {
+      list.set(i, (float) (list.get(i) * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+    }
+  }
+
 
   private static void methodA() {
     var list = new ArrayList<Float>(SIZE);
@@ -12,9 +23,7 @@ public class Main {
       list.add(1.0f);
     }
     long startTime = System.currentTimeMillis();
-    for (int i = 0; i < list.size(); i++) {
-      list.set(i, (float) (list.get(i) * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-    }
+    calculate(list);
     System.out.printf("One thread time: %d ms.\n", System.currentTimeMillis() - startTime);
   }
 
@@ -26,29 +35,22 @@ public class Main {
     long startTime = System.currentTimeMillis();
     var subList1 = new ArrayList<>(list.subList(0, HALF));
     var subList2 = new ArrayList<>(list.subList(HALF + 1, list.size()));
-    var thread1 = new Thread(() -> {
-      for (int i = 0; i < subList1.size(); i++) {
-        subList1.set(i, (float) (subList1.get(i) * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-      }
-    }, "Thread 1");
-    var thread2 = new Thread(() -> {
-      for (int i = 0; i < subList2.size(); i++) {
-        subList2.set(i, (float) (subList2.get(i) * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-      }
-    });
+    var thread1 = new Thread(() -> calculate(subList1), "Thread 1");
+    var thread2 = new Thread(() -> calculate(subList2), "Thread 2");
     thread1.start();
     thread2.start();
     try {
       thread1.join();
       thread2.join();
     } catch (InterruptedException e) {
-      e.printStackTrace();
+      LOGGER.log(Level.SEVERE, "An error occurred while calculating the list", e);
     }
     var resultList = new ArrayList<Float>(SIZE);
     resultList.addAll(subList1);
     resultList.addAll(subList2);
     System.out.printf("Two thread time: %d ms.\n", System.currentTimeMillis() - startTime);
   }
+
 
   public static void main(String[] args) {
     methodA();
